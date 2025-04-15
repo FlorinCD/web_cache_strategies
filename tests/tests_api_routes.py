0ae5a4e0dd02bc1f66436a4a8588b361af81d4ee
Florin <florin.cosminn22@gmail.com>
2025-04-15 21:14:01 +0300

    added new tests for the endpoints: cache_first_stats, network_first_stats, stale_while_revalidate_stats, cache_test

--- tests/tests_api_routes.py
+++ tests/tests_api_routes.py
@@ -40,3 +40,52 @@ def test_cache_first(client_cleared_cache):
         assert "source" in response_dict
         assert response_dict["source"] == "cache"
 
+
+def test_strategy_cache_first_stats(client_cleared_cache):
+    try:
+        response_json = client_cleared_cache.get('/cache_first_stats').data
+        response = json.loads(response_json)
+    except Exception as e:
+        assert False, f"Failed of: {e}"
+    assert "hits" in response
+    assert "misses" in response
+    assert "latency_hits" in response
+    assert "latency_misses" in response
+
+
+def test_strategy_network_first_stats(client_cleared_cache):
+    try:
+        response_json = client_cleared_cache.get('/network_first_stats').data
+        response = json.loads(response_json)
+    except Exception as e:
+        assert False, f"Failed of: {e}"
+
+    assert "updates" in response
+    assert "latency_network" in response
+
+
+def test_strategy_stale_while_revalidate_stats(client_cleared_cache):
+    try:
+        response_json = client_cleared_cache.get('/stale_while_revalidate_stats').data
+        response = json.loads(response_json)
+    except Exception as e:
+        assert False, f"Failed of: {e}"
+
+    assert "hits" in response
+    assert "misses" in response
+    assert "updates" in response
+    assert "latency_stale" in response
+    assert "latency_misses" in response
+
+
+def test_clear_cache(client_cleared_cache, app_instance):
+    try:
+        response_json = client_cleared_cache.get('/cache_test/user1')
+        cached_value = app_instance.cache.get("user1") or 'false'
+
+        clear_response = client_cleared_cache.get('/clear-cache')
+        cached_value = app_instance.cache.get("user1") or 'false'
+        assert cached_value == 'false'
+    except Exception as e:
+        assert False, f"Failed of: {e}"
+
